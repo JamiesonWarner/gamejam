a8cec1c803dce573b1f76fa847c1c5cfe824b99c
Jamieson Warner <jamiesonwarner@gmail.com>
2014-04-04 21:31:27 -0500

    avoid namespace conflict

--- app/ai.js
+++ app/ai.js
@@ -5,11 +5,11 @@ function AIController(game, enemies, player, obstacles) {
     this.obstacles = obstacles;
 
     this.ENEMY_SPEED = 5;
-    this.ENEMY_ACCELERATION = .1;
+    this.ENEMY_aiAcceleration = .1;
 }
 
 AIController.prototype.update = function () {
-    // Add acceleration vector to enemies if they don't have one already
+    // Add aiAcceleration vector to enemies if they don't have one already
     for (var i = 0; i < this.enemies.length; i++) {
         var enemy = this.enemies[i];
         this.update_enemy(enemy);
@@ -17,26 +17,26 @@ AIController.prototype.update = function () {
 }
 
 AIController.prototype.update_enemy = function(enemy) {
-    if (!enemy.velocity) {
-        enemy.velocity = new Phaser.Point(0,0);
+    if (!enemy.aiVelocity) {
+        enemy.aiVelocity = new Phaser.Point(0,0);
         enemy.x = this.game.rnd.integerInRange(0,this.game.width);
         enemy.y = this.game.rnd.integerInRange(0,this.game.height);
 
-        enemy.acceleration = new Phaser.Point();
+        enemy.aiAcceleration = new Phaser.Point();
     }
 
     var player_location = new Phaser.Point(this.game.input.x,this.game.input.y);
 
-    enemy.acceleration.x = player_location.x - enemy.x;
-    enemy.acceleration.y = player_location.y - enemy.y;
-    enemy.acceleration.setMagnitude(this.ENEMY_ACCELERATION);
+    enemy.aiAcceleration.x = player_location.x - enemy.x;
+    enemy.aiAcceleration.y = player_location.y - enemy.y;
+    enemy.aiAcceleration.setMagnitude(this.ENEMY_aiAcceleration);
 
-    enemy.velocity = Phaser.Point.add(enemy.acceleration, enemy.velocity, enemy.velocity);
-    if (enemy.velocity.getMagnitude() > this.ENEMY_SPEED) {
-        enemy.velocity.setMagnitude(this.ENEMY_SPEED);
+    enemy.aiVelocity = Phaser.Point.add(enemy.aiAcceleration, enemy.aiVelocity, enemy.aiVelocity);
+    if (enemy.aiVelocity.getMagnitude() > this.ENEMY_SPEED) {
+        enemy.aiVelocity.setMagnitude(this.ENEMY_SPEED);
     }
 
-    enemy.x += enemy.velocity.x;
-    enemy.y += enemy.velocity.y;
+    enemy.x += enemy.aiVelocity.x;
+    enemy.y += enemy.aiVelocity.y;
 
 }
