584d30e95aaaf11ba8700bf9473c19cef5e3b785
Jamieson Warner <jamiesonwarner@gmail.com>
2014-04-04 20:20:04 -0500

    first ai module commit

--- app/ai.js
+++ app/ai.js
@@ -1,10 +1,31 @@
-function AIController(enemies, player, obstacles) {
+function AIController(game, enemies, player, obstacles) {
+    this.game = game;
     this.enemies = enemies;
     this.player = player;
     this.obstacles = obstacles;
+
+    this.ENEMY_SPEED = 5;
 }
 
 AIController.prototype.update = function () {
-    // 
+    // Add acceleration vector to enemies if they don't have one already
+    for (var i = 0; i < this.enemies.length; i++) {
+        var enemy = enemies[i];
+        this.update_enemy(enemy);
+    }
+}
+
+AIController.prototype.update_enemy = function(enemy) {
+    if (!enemy.velocity) {
+        enemy.velocity = new Point(0,0);
+    }
+
+    var player_location = new Point(0,0);
+    enemy.velocity = player_location.subtract(enemy.velocity,enemy.world).
+                        normalize().
+                        multiply(new Point(this.ENEMY_SPEED, this.ENEMY_SPEED));
+
+    enemy.x += enemy.velocity.x;
+    enemy.y += enemy.velocity.y;
 
 }
