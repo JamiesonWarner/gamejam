33063421c0a29dad799504a23161d9811d2c370f
Max Horowitz-Gelb <mhorowitzgelb@hotmail.com>
2014-04-04 21:42:11 -0500

    Fixed player bug

--- app/main.js
+++ app/main.js
@@ -10,15 +10,19 @@
         game.load.image('logo', 'phaser.png');
         game.load.image('enemy','img/enemy_test.png');
         game.load.image('player','img/player.jpeg');
-        ai = new AIController(game,enemies,player,obstacles);
-        player = new Player(game,player_sprite);
+        
     }
 
     function create () {
+		player_sprite = game.add.sprite(game.world.centerX,game.world.centerY,'player');
+        game.physics.enable([player_sprite],Phaser.Physics.ARCADE);
+		player = new Player(game,player_sprite);
+		
+    	ai = new AIController(game,enemies,player,obstacles);
+        
         var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
         logo.anchor.setTo(0.5, 0.5);
-        player_sprite = game.add.sprite(game.world.centerX,game.world.centerY,'player');
-        game.physics.enable([player_sprite],Phaser.Physics.ARCADE);
+       
         for(var i = 0; i < 10; i ++){
         	enemies[i] = game.add.sprite(game.world.bounds.x / 10, game.world.centerY,'enemy' );
         }
--- app/player.js
+++ app/player.js
@@ -4,8 +4,8 @@ function Player(game, player ){
 }
 
 Player.prototype.update = function(){
-	game.physics.arcade.moveToPointer(player,400);
-	if(Phaser.Rectangle.contains(player.body,game.input.x, game.input.y)){
-		player.body.velocity.setTo(0,0);
+	this.game.physics.arcade.moveToPointer(this.player,400);
+	if(Phaser.Rectangle.contains(this.player.body,this.game.input.x, this.game.input.y)){
+		this.player.body.velocity.setTo(0,0);
 	}
 }
