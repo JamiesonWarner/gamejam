2e1247056ca4250f9f49ff562081a87d1ca195cf
Max Horowitz-Gelb <mhorowitzgelb@hotmail.com>
2014-04-04 23:52:13 -0500

    set up player death and enemy collisions

--- app/main.js
+++ app/main.js
@@ -6,36 +6,90 @@
     var obstacles = [];
     var ai;
     var player_sprite;
+    var lives = 3;
+    var livesText;
     function preload () {
         game.load.image('logo', 'phaser.png');
         game.load.image('enemy','img/enemy_test.png');
-        game.load.image('player','img/player.jpeg');
+        game.load.image('player','img/player.png');
         
     }
 
     function create () {
     	var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
         logo.anchor.setTo(0.5, 0.5);
-		player_sprite = game.add.sprite(game.world.centerX,game.world.centerY,'player');
+        game.world.setBounds(0,0,2000,2000);
+		resetSprites();
+        livesText = game.add.text(0,0,"Lives: "+ lives,{
+        font: "24px Arial",
+        fill: "#ff0044",
+        align: "center"
+    });
+    }
+
+    function update(){
+    	for(var i = 0; i < enemies.length ; i ++){
+    		game.physics.arcade.collide(player_sprite,enemies[i],killPlayer,null,this);
+    	}
+
+    	for(var i = 0; i < enemies.length; i ++){
+    		for(var j =  i + 1; j < enemies.length; j ++){
+    			game.physics.arcade.collide(enemies[i],enemies[j],enemyCollide,null,this);
+    		}
+    	}
+    	ai.update();
+    	player.update();
+    }
+
+    function killPlayer(player,enemy){
+    	for(var i = 0; i < enemies.length; i++){
+    		enemies[i].destroy();
+    	}
+    	player_sprite.destroy();
+    	lives --;
+    	livesText.setText("Lives: "+lives);
+    	if(lives == 0){
+    		game.add.text(game.world.centerX,game.world.centerY,"You Lose!", {font: "64px Arial",
+        fill: "#ff0044",
+        align: "center"});
+    	}
+    	else{
+    		game.time.events.add(Phaser.Timer.SECOND * 4,resetSprites,this);
+    	}	
+    }
+
+    function resetSprites(){
+    	player_sprite = game.add.sprite(game.world.centerX,game.world.centerY,'player');
         game.physics.enable([player_sprite],Phaser.Physics.ARCADE);
 		player = new Player(game,player_sprite);
-
+		game.camera.follow(player_sprite);
     	ai = new AIController(game,enemies,player,obstacles);
         
         
        
         for(var i = 0; i < 10; i ++){
-        	enemies[i] = game.add.sprite(game.world.bounds.x / 10, game.world.centerY,'enemy' );
+        	var x = 400;
+        	var y =  400;
+        	while((x > 0 && x < game.world.width) ||  (y > 0 && y < 600)){
+        		x = game.rnd.integerInRange(-800, 1600);
+        		y =game.rnd.integerInRange(-600,1200);
+        	} 
+        	enemies[i] = game.add.sprite(x,y,'enemy' );
+        
         }
-        game.physics.enable(enemies,Phaser.Physics.ARCADE);
+        game.physics.enable(enemies,Phaser.Physics.ARCADE);	
     }
 
-    function update(){
-    	for(var i = 0; i < 10; i ++){
-    		game.physics.arcade.collide(player_sprite,enemies[i],null,null,this);
-    	}
-    	ai.update();
-    	player.update();
+    function enemyCollide(enemy1, enemy2){
+    	replaceEnemy(enemy1);
+    	replaceEnemy(enemy2);
+    }
+
+    function replaceEnemy(enemy){
+    	while((enemy.body.x > 0 && enemy.body.x < game.world.width) ||  (enemy.body.y > 0 && enemy.body.y < 600)){
+        		enemy.body.x = game.rnd.integerInRange(-800, 1600);
+        		enemy.body.y =game.rnd.integerInRange(-600,1200);
+        } 	
     }
 
 
