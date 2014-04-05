95035a93c87ddd73c5e7de1162350b272ddc257c
Jamieson Warner <jamiesonwarner@gmail.com>
2014-04-04 20:00:09 -0500

    added ai module

---
+++ app/ai.js
@@ -0,0 +1,10 @@
+function AIController(enemies, player, obstacles) {
+    this.enemies = enemies;
+    this.player = player;
+    this.obstacles = obstacles;
+}
+
+AIController.prototype.update = function () {
+    // 
+
+}
